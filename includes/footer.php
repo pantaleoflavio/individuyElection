1506b9d716c6194d08fc2ea4e873a5f07e412904
flaviopantaleo <flavio.pantaleo@yahoo.com>
2024-04-19 20:04:51 +0200

    add missing files

--- includes/footer.php
+++ includes/footer.php
@@ -1,19 +1,19 @@
     <!-- Footer -->
     <footer class="text-center text-lg-start bg-secondary">
     <div class="container d-flex justify-content-center py-5">
-      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
-        <i class="fab fa-facebook-f"></i>
-      </button>
-      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
-        <i class="fab fa-youtube"></i>
-      </button>
-      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
-        <i class="fab fa-instagram"></i>
-      </button>
-      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
-        <i class="fab fa-twitter"></i>
-      </button>
-    </div>
+      <a title="forum individuy italiani" target="_blank" href="https://indyviduitaliani.forumcommunity.net/" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+          <i class="fab fa-wordpress"></i>
+      </a>
+      <a title="nostro canale youtube" target="_blank" href="https://youtube.com/@indyviduichannel3919?si=PARsIaG31J4sC-Um" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+          <i class="fab fa-youtube"></i>
+      </a>
+      <a title="gruppo whatsapp" target="_blank" href="https://chat.whatsapp.com/IthimX4dVEvBhE5iA1wqbt" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+          <i class="fab fa-whatsapp"></i>
+      </a>
+      <a target="_blank" href="https://www.twitter.com/yourProfile" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+          <i class="fab fa-twitter"></i>
+      </a>
+  </div>
 
     <!-- Copyright -->
     <div class="text-center text-white p-3" style="background-color: rgba(0, 0, 0, 0.2);">
